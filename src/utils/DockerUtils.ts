c0362037a8106c9c23be796cf5444456d90a2497
refracta <58779799+refracta@users.noreply.github.com>
2023-04-28 10:56:43 +0900

    Add loadImage function

--- src/utils/DockerUtils.ts
+++ src/utils/DockerUtils.ts
@@ -1,10 +1,37 @@
-import {HTTPService} from "../service/interfaces/http/HTTPService";
-import {Image} from "../entities/Image";
-import * as Dockerode from "dockerode";
+import * as Dockerode from 'dockerode';
 
 export class DockerUtils {
-    static async createContainer(image: Image) {
-        const docker = new Dockerode(image.region);
-        // const container = await docker.createContainer({Image});
+    static async loadImage(docker:Dockerode, path: string, tagOptions) {
+        await new Promise((resolve, reject) => {
+            let importedIdentifier;
+            docker.getEvents(function (error, eventStream) {
+                eventStream.on('data', async (data) => {
+                    const event = JSON.parse(data.toString());
+                    if (event.Type === 'image' && event.Action === 'load') {
+                        const image = await docker.getImage(event.id);
+                        await image.tag(tagOptions);
+                        const originalTagImage = await docker.getImage(importedIdentifier);
+                        await originalTagImage.remove();
+
+                        eventStream.removeAllListeners();
+                        eventStream.unpipe();
+
+                        resolve(image);
+                    }
+                });
+            });
+
+            docker.loadImage(path, {}, (error, stream) => {
+                if (error) {
+                    return reject(error);
+                }
+                docker.modem.followProgress(stream, (err, res) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    importedIdentifier = res[0].stream.split(':').slice(1).join(':').trim();
+                });
+            });
+        });
     }
 }
