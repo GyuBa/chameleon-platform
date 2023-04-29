a3480c73f52c2be2d5829c5a454badc7d37cc2cb
refracta <58779799+refracta@users.noreply.github.com>
2023-04-30 07:05:26 +0900

    Add exec function

--- src/utils/DockerUtils.ts
+++ src/utils/DockerUtils.ts
@@ -1,7 +1,22 @@
 import * as Dockerode from 'dockerode';
+import {Container} from "dockerode";
 
 export class DockerUtils {
-    static async loadImage(docker:Dockerode, path: string, tagOptions) {
+
+    static exec(container: Container, command: string) {
+        return new Promise(resolve => {
+            container.exec({
+                Cmd: ['/bin/bash', '-c', command],
+                AttachStdin: true,
+                AttachStdout: true
+            }, async (err, exec) => {
+                const stream = await exec?.start({hijack: true, stdin: true});
+                stream?.on('finish', resolve);
+            });
+        });
+    }
+
+    static async loadImage(docker: Dockerode, path: string, tagOptions) {
         await new Promise((resolve, reject) => {
             let importedIdentifier;
             docker.getEvents(function (error, eventStream) {
