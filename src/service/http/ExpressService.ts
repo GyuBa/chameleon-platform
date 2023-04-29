158b6e61c7b1bf08a363ba7bbe734b180f3729fe
refracta <58779799+refracta@users.noreply.github.com>
2023-04-30 03:31:02 +0900

    Remove express-fileupload

--- src/service/http/ExpressService.ts
+++ src/service/http/ExpressService.ts
@@ -2,14 +2,12 @@ import * as express from 'express';
 import {Application} from 'express';
 import {HTTPService} from '../interfaces/http/HTTPService';
 import {Server} from 'http';
-import * as fileUpload from 'express-fileupload';
 import * as session from 'express-session';
 import {TypeormStore} from 'connect-typeorm';
 
 export class ExpressService extends HTTPService {
     init(app: Application, server: Server) {
         app.use(express.json());
-        app.use(fileUpload());
         app.use(session({
             resave: false,
             saveUninitialized: false,
