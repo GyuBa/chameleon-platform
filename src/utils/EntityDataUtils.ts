cbfdacb63671901bc728baa858165d8c3332ad60
refracta <58779799+refracta@users.noreply.github.com>
2023-05-04 10:15:05 +0900

    Remove EntityDataUtils.ts

--- src/utils/EntityDataUtils.ts
+++
@@ -1,8 +0,0 @@
-export class EntityDataUtils {
-    static toData(keys: string[], entity: any) {
-        return keys.reduce((obj, key) => ({
-            ...obj,
-            [key]: entity[key]?.toData ? entity[key].toData() : entity[key]
-        }), {});
-    }
-}
