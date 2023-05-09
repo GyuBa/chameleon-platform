f2918b2de2ec8c5d32dd7895b1dbc14fdd3e7d48
refracta <58779799+refracta@users.noreply.github.com>
2023-05-10 00:33:42 +0900

    Migrate to PlatformAPI

--- src/__tests__/ExecuteModel.test.ts
+++ src/__tests__/ExecuteModel.test.ts
@@ -1,9 +1,9 @@
 import {TestingManager} from './common/TestingManager';
 import {LoginUtils} from './common/LoginUtils';
 import * as fs from 'fs';
-import * as FormData from 'form-data';
 import PlatformServer from '../server/core/PlatformServer';
 import {ModelController} from '../controller/ModelController';
+import {PlatformAPI} from "../platform/PlatformAPI";
 
 function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
@@ -18,17 +18,17 @@ describe('Execute model', () => {
 
     // (chameleon-platform root에서) curl -o test.tar http://files.chameleon.best/images/simple-output-image.tar
     test('upload', async () => {
-        const formData = new FormData();
-        formData.append('regionName', 'mongle');
-        formData.append('modelName', modelName);
-        formData.append('description', '# test model description');
-        formData.append('inputType', 'image');
-        formData.append('outputType', 'image');
-        formData.append('parameters', JSON.stringify({uischema: {}, schema: {}}));
-        formData.append('file', fs.createReadStream('test.tar'));
-        // 주의: Front-end 에서는 fs 모듈이 없으므로 다른 방식으로 처리해야 함
         try {
-            const result = await TestingManager.axios.post('/models/upload', formData).then(r => r.data);
+            const result = await PlatformAPI.uploadModel({
+                regionName: 'mongle',
+                modelName,
+                description: '# test model description',
+                inputType: 'image',
+                outputType: 'image',
+                parameters: JSON.stringify({uischema: {}, schema: {}}),
+                file: fs.createReadStream('test.tar') as any
+                // 주의: Front-end 에서는 fs 모듈이 없으므로 다른 방식으로 처리해야 함
+            });
             console.log(result);
         } catch (e) {
             fail(e.response.data);
@@ -40,14 +40,12 @@ describe('Execute model', () => {
         const modelController = new ModelController(PlatformServer.source);
         const model = await modelController.findByUniqueName(modelName);
 
-        const formData = new FormData();
-        formData.append('modelId', model.id);
-        formData.append('parameters', JSON.stringify({param1: 'example1', param2: 2}));
-        // JSONForm의 결과물
-        formData.append('input', fs.createReadStream('image.png'));
-        // 주의: Front-end 에서는 fs 모듈이 없으므로 다른 방식으로 처리해야 함
         try {
-            const result = await TestingManager.axios.post('/models/execute', formData).then(r => r.data);
+            const result = PlatformAPI.executeModel({
+                modelId: model.id,
+                parameters: JSON.stringify({param1: 'example1', param2: 2}),
+                input: fs.createReadStream('image.png') as any
+            });
             console.log(result);
         } catch (e) {
             fail(e.response.data);
