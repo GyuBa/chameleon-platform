20b170b98e12ae6f2a7923103eda0a1bbf74c258
refracta <58779799+refracta@users.noreply.github.com>
2023-05-10 00:32:30 +0900

    Fix init function

--- src/__tests__/common/TestingManager.ts
+++ src/__tests__/common/TestingManager.ts
@@ -2,18 +2,24 @@ import axios from "axios";
 import PlatformServer from "../../server/core/PlatformServer";
 import {wrapper} from 'axios-cookiejar-support';
 import {CookieJar} from 'tough-cookie';
+import {PlatformAPI} from "../../platform/PlatformAPI";
+import * as LocalFormData from 'form-data';
 
 export class TestingManager {
-    static axios;
-
     static async init() {
         PlatformServer.loadConfig();
         PlatformServer.init({});
         await PlatformServer.initializeDB(true);
-        this.axios = wrapper(axios.create({
+        PlatformAPI.instance = wrapper(axios.create({
             baseURL: 'http://localhost:' + PlatformServer.config.httpPort,
-            /*timeout: 1000,*/ jar: new CookieJar()
+            timeout: 3000, jar: new CookieJar()
         }));
+
+        PlatformAPI.toFormData = (data: any) => {
+            const formData = new LocalFormData();
+            Object.entries(data).forEach(([name, value]: [string, string | Blob]) => formData.append(name, value));
+            return formData as any;
+        };
     }
 }
 
