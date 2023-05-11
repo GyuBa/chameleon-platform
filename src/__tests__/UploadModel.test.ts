e6edf01164da51c29318c66443070a05d452ae90
refracta <58779799+refracta@users.noreply.github.com>
2023-05-11 22:15:51 +0900

    Add upload model test

---
+++ src/__tests__/UploadModel.test.ts
@@ -0,0 +1,53 @@
+import {TestingManager} from './common/TestingManager';
+import {LoginUtils} from './common/LoginUtils';
+import * as fs from 'fs';
+import {PlatformAPI} from '../platform/PlatformAPI';
+
+function sleep(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+const modelName = `test-model-${new Date().getTime()}`;
+describe('Upload model', () => {
+    beforeAll(async () => {
+        await TestingManager.init();
+        await LoginUtils.login();
+    });
+
+    // (chameleon-platform root에서) curl -o test.tar http://files.chameleon.best/images/simple-output-image.tar
+    test('upload image', async () => {
+        try {
+            const result = await PlatformAPI.uploadModelWithImage({
+                regionName: 'mongle',
+                modelName,
+                description: '# test model description',
+                inputType: 'image',
+                outputType: 'image',
+                parameters: JSON.stringify({uischema: {}, schema: {}}),
+                file: fs.createReadStream('test.tar') as any
+                // 주의: Front-end 에서는 fs 모듈이 없으므로 다른 방식으로 처리해야 함
+            });
+            console.log(result);
+        } catch (e) {
+            fail(e.response.data);
+        }
+    }, 2 * 60 * 1000);
+
+    test('upload dockerfile', async () => {
+        try {
+            const result = await PlatformAPI.uploadModelWithDockerfile({
+                regionName: 'mongle',
+                modelName,
+                description: '# test model description',
+                inputType: 'image',
+                outputType: 'image',
+                parameters: JSON.stringify({uischema: {}, schema: {}}),
+                files: [fs.createReadStream('Dockerfile') as any]
+                // 주의: Front-end 에서는 fs 모듈이 없으므로 다른 방식으로 처리해야 함
+            });
+            console.log(result);
+        } catch (e) {
+            fail(e.response.data);
+        }
+    }, 2 * 60 * 1000);
+});
