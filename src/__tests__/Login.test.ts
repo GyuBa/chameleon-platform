a2054c74271e4862e40c10598017ac682f20766c
refracta <58779799+refracta@users.noreply.github.com>
2023-05-11 22:19:08 +0900

    Add sign out test

--- src/__tests__/Login.test.ts
+++ src/__tests__/Login.test.ts
@@ -1,6 +1,5 @@
-import PlatformServer from '../server/core/PlatformServer';
-import {UserController} from '../controller/UserController';
 import {TestingManager} from './common/TestingManager';
+import {PlatformAPI} from "../platform/PlatformAPI";
 
 const testAccount = {
     'email': `test@test.com`,
@@ -11,24 +10,28 @@ const testAccount = {
 describe('Login', () => {
     beforeAll(async () => await TestingManager.init());
 
-/*    afterAll(async () => {
-        const userController = new UserController(PlatformServer.source);
-        const testUser = await userController.findUserByEmail(testAccount.email);
-        if (testUser) {
-            await userController.delete(testUser);
-        }
-    });*/
+    /*
+    afterAll(async () => {
+            const userController = new UserController(PlatformServer.source);
+            const testUser = await userController.findUserByEmail(testAccount.email);
+            if (testUser) {
+                await userController.delete(testUser);
+            }
+        });
+    */
 
     test('sign-up', async () => {
-        const result = await TestingManager.axios.post('/auth/sign-up', testAccount).then(r => r.data);
+        const result = await PlatformAPI.signUp(testAccount.email, testAccount.password, testAccount.username);
         console.log(result);
     });
 
     test('sign-in', async () => {
-        const result = await TestingManager.axios.post('/auth/sign-in', testAccount).then(r => ({
-            data: r.data,
-            cookie: r.headers['set-cookie'][0]
-        }));
+        const result = await PlatformAPI.signIn(testAccount.email, testAccount.password);
+        console.log(result);
+    });
+
+    test('sign-out', async () => {
+        const result = await PlatformAPI.signOut();
         console.log(result);
     });
 });
