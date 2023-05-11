4c2210451f9c33b387634752912288e6f0f00d07
refracta <58779799+refracta@users.noreply.github.com>
2023-05-11 22:17:52 +0900

    Fix sign out bug

--- src/service/http/AuthService.ts
+++ src/service/http/AuthService.ts
@@ -3,10 +3,10 @@ import {Application, Request, Response} from 'express';
 import * as bcrypt from 'bcrypt';
 import * as passport from 'passport';
 import {User} from '../../entities/User';
-import {RESPONSE_MESSAGE} from '../../constant/Constants';
 import {HTTPService} from '../interfaces/http/HTTPService';
 import {Server} from 'http';
-import {HTTPLogUtils} from "../../utils/HTTPLogUtils";
+import {HTTPLogUtils} from '../../utils/HTTPLogUtils';
+import {ResponseData} from '../../types/chameleon-platform.common';
 
 export class AuthService extends HTTPService {
     init(app: Application, server: Server) {
@@ -32,13 +32,11 @@ export class AuthService extends HTTPService {
      */
     async handleSignUp(req: Request, res: Response, next: Function) {
         if (!(req.body.username && req.body.password && req.body.email)) {
-            res.status(401).send(RESPONSE_MESSAGE.NON_FIELD);
-            return;
+            return res.status(401).send({msg: 'non_field_error'} as ResponseData);
         }
 
         if (await this.userController.findByEmail(req.body.email)) {
-            res.status(401).send(RESPONSE_MESSAGE.DUPLICATED_EMAIL);
-            return;
+            return res.status(401).send({msg: 'duplicated_email_error'} as ResponseData);
         }
         const {email, username} = req.body;
         const password = await bcrypt.hashSync(req.body.password, await bcrypt.genSaltSync());
@@ -47,15 +45,7 @@ export class AuthService extends HTTPService {
         user.username = username;
         user.password = password;
         await this.userController.save(user);
-        res.status(200).send(RESPONSE_MESSAGE.OK);
-    }
-
-    async handleLegacyInfo(req: Request, res: Response, next: Function) {
-        if (req.isAuthenticated()) {
-            res.status(200).send((req.user as User).toData());
-        } else {
-            res.status(401).send(RESPONSE_MESSAGE.NOT_AUTH);
-        }
+        res.status(200).send({msg: 'ok'} as ResponseData);
     }
 
     handleSignIn(req: Request, res: Response, next: Function) {
@@ -79,10 +69,10 @@ export class AuthService extends HTTPService {
 
     async handleModifyPassword(req: Request, res: Response, next: Function) {
         if (!req.isAuthenticated()) {
-            return res.status(401).send(RESPONSE_MESSAGE.NOT_AUTH);
+            return res.status(401).send({msg: 'not_authenticated_error'} as ResponseData);
         }
         if (!(req.body.password)) {
-            return res.status(401).send(RESPONSE_MESSAGE.NON_FIELD);
+            return res.status(401).send({msg: 'non_field_error'} as ResponseData);
         }
         const password = await bcrypt.hashSync(req.body.password, await bcrypt.genSaltSync());
         console.log(req.user);
@@ -90,9 +80,9 @@ export class AuthService extends HTTPService {
             const user = new User();
             user.password = password;
             await this.userController.save(user);
-            return res.status(200).send(RESPONSE_MESSAGE.OK);
+            return res.status(200).send({msg: 'ok'} as ResponseData);
         } catch (e) {
-            return res.status(501).send(RESPONSE_MESSAGE.SERVER_ERROR);
+            return res.status(501).send({msg: 'server_error'} as ResponseData);
         }
     }
 
@@ -100,17 +90,18 @@ export class AuthService extends HTTPService {
     async handleSignOut(req: Request, res: Response, next: Function) {
         let isSessionError = false;
 
-        if (!req.isAuthenticated()) return res.status(401).send(RESPONSE_MESSAGE.NOT_AUTH);
-        await req.session.destroy(err => {
-            if (err) {
+        if (!req.isAuthenticated()) return res.status(401).send({msg: 'not_authenticated_error'} as ResponseData);
+        await req.session.destroy(error => {
+            if (error) {
                 isSessionError = true;
+                console.error(error);
             }
         });
 
-        if (isSessionError) {
-            return res.status(200).send(RESPONSE_MESSAGE.OK);
+        if (!isSessionError) {
+            return res.status(200).send({msg: 'ok'} as ResponseData);
         } else {
-            return res.status(501).send(RESPONSE_MESSAGE.SERVER_ERROR);
+            return res.status(501).send({msg: 'server_error'} as ResponseData);
         }
     }
 }
