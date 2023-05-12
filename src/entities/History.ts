065c10d6c5acc8127ba56765cb3e7257e01be5d3
refracta <58779799+refracta@users.noreply.github.com>
2023-05-12 23:54:20 +0900

    Add inputType, outputType to column

--- src/entities/History.ts
+++ src/entities/History.ts
@@ -2,10 +2,16 @@ import {Column, Entity, JoinColumn, ManyToOne} from 'typeorm';
 import {Common} from './interfaces/Common';
 import {Model} from './Model';
 import {User} from './User';
-import {HistoryStatus, ModelInputInfo, ModelOutputInfo} from '../types/chameleon-platform.common';
+import {
+    HistoryStatus,
+    ModelInputInfo,
+    ModelInputType,
+    ModelOutputInfo,
+    ModelOutputType
+} from '../types/chameleon-platform.common';
 
 @Entity()
-export class History extends Common{
+export class History extends Common {
     @Column({type: 'enum', enum: HistoryStatus})
         status: string;
     @Column()
@@ -14,11 +20,15 @@ export class History extends Common{
         inputPath: string;
     @Column({nullable: true, type: 'json'})
         inputInfo: ModelInputInfo;
+    @Column({nullable: true, enum: ModelInputType})
+        inputType: string;
 
     @Column({nullable: true})
         outputPath: string;
     @Column({nullable: true, type: 'json'})
         outputInfo: ModelOutputInfo;
+    @Column({nullable: true, enum: ModelOutputType})
+        outputType: string;
     @Column({nullable: true, type: 'text'})
         description: string;
     @ManyToOne(
