16e43de24860e79afa46c47d1a508ba791c6efd7
refracta <58779799+refracta@users.noreply.github.com>
2023-05-17 09:22:20 +0900

    Add getMulterLikeFilePath

--- src/utils/MulterUtils.ts
+++ src/utils/MulterUtils.ts
@@ -1,6 +1,20 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+
 export class MulterUtils {
     static fixNameEncoding(req, file, cb) {
         file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
         cb(null, true);
+    }
+
+    static getMulterLikeFilePath(basePath: string) {
+        let path;
+        for (; ;) {
+            path = `${basePath}/${crypto.randomBytes(16).toString('hex')}`;
+            if (!fs.existsSync(path)) {
+                break;
+            }
+        }
+        return path;
     }
 }
